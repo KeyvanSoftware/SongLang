aba52c15c57730e2df2e8d56b22a7015c4b200e4
Keyvan Khezri <keyvan.khezri@outlook.com>
2025-09-03 17:46:04 +0100

    Add utils.py, detect language using lyrics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from langdetect import detect
 from spotify_client import create_spotify_client
 from genius_client import get_lyrics
+from utils import detect_language
 
 # Create spotify client
 sp = create_spotify_client()
@@ -11,10 +11,11 @@ print(f"Logged in as: {user['display_name']} ({user['id']})\n")
 
 # --- Fetch first 5 liked songs ---
 print("Your first 5 liked songs:")
-saved_tracks = sp.current_user_saved_tracks(limit=1)
+saved_tracks = sp.current_user_saved_tracks(limit=5)
 for idx, item in enumerate(saved_tracks['items']):
     track = item['track']
     print(f"{idx+1}. {track['name']} - {track['artists'][0]['name']}")
     lyrics = get_lyrics(track['name'], track['artists'][0]['name'])
-    print(lyrics)
+    language = detect_language(lyrics)
+    print("Language: ", language)
 print("\n")
