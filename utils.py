a900068f97e62cb30198c91f8f238a0d1b13a793
Keyvan Khezri <keyvan.khezri@outlook.com>
2025-09-04 15:44:00 +0100

    feat: group songs into a dictionary of languages

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 from langdetect import detect
+from collections import defaultdict
+
+songs_by_language = defaultdict(list) 
 
 def detect_language(text):
     try:
         return detect(text)
     except:
         return None
+
+
+def group_by_language(language, uri):
+    if language != 'en':
+        songs_by_language[language].append(uri)
